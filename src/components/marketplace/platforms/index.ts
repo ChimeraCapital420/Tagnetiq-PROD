2814cae49b02dffb9cfb651fb0540e5b173340d9
ChimeraCapital420 <projectchimera88@gmail.com>
2026-01-28 12:09:45 -0700

    fix: add platforms/index.ts

---
+++ src/components/marketplace/platforms/index.ts
@@ -0,0 +1,6 @@
+// FILE: src/components/marketplace/platforms/index.ts
+// Re-exports all platform-related modules
+
+export * from './types';
+export * from './conditionMaps';
+export * from './platformConfigs';
