fc0e1a4d6fd4efcdb95466aeb00f13ea15c31358
ChimeraCapital420 <projectchimera88@gmail.com>
2026-02-25 22:34:05 -0700

    fix: register MetaGlassesPlugin in MainActivity + BT warning fix
    - Added registerPlugin(MetaGlassesPlugin.class) to MainActivity.onCreate
    - Plugin was compiling but never registered with Capacitor bridge
    - Hide Web Bluetooth warning inside Capacitor native shell
    - Switch minify from terser to esbuild (fixes OOM builds)

--- android/app/src/main/java/com/tagnetiq/app/MainActivity.java
+++ android/app/src/main/java/com/tagnetiq/app/MainActivity.java
@@ -1,5 +1,13 @@
-package com.tagnetiq.app;
+﻿package com.tagnetiq.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import com.tagnetiq.plugins.metaglasses.MetaGlassesPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        registerPlugin(MetaGlassesPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
